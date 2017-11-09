c8c777d3e82a6fbd2b839c2c7af8971f7c67f63d
Thanos Kalogiratos <thanosk97@gmail.com>
2017-11-09 14:47:19 +0000

    validation page

--- Stage One/HTML Mock-Ups/User Registration Working Prototype/Javascript/UserValidation.js
+++ Stage One/HTML Mock-Ups/User Registration Working Prototype/Javascript/UserValidation.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     ConfigureLogin();
     ConfigureRegister();
+    ConfigureForgotUsername();
+    ConfigureForgotPassword();
 }
 
 window.onclick = function () {
@@ -20,6 +22,8 @@ var ConfigureLogin = function () {
     var password = document.getElementById("logPassword");
     var submit = document.getElementById("logSubmit");
     var noAccount = document.getElementById("logRegister");
+    var forgotUsername = document.getElementById("forgotUsername");
+    var forgotPassword = document.getElementById("forgotPassword");
 
     noAccount.onclick = function () {
         document.getElementById("regDiv").style["display"] = "block";
@@ -27,11 +31,26 @@ var ConfigureLogin = function () {
 
         login.value = "";
         password.value = "";
-        document.getElementById("regSubmit").style["display"] = "block";
-        document.getElementById("regCancel").value = "Cancel";
+    }
+
+    forgotUsername.onclick = function () {
+        document.getElementById("forgotUsernameDiv").style["display"] = "block";
+        document.getElementById("loginDiv").style["display"] = "none";
+
+        login.value = "";
+        password.value = "";
+    }
+
+    forgotPassword.onclick = function () {
+        document.getElementById("forgotPasswordDiv").style["display"] = "block";
+        document.getElementById("loginDiv").style["display"] = "none";
+
+        login.value = "";
+        password.value = "";
     }
 
     submit.onclick = function () {
+
         firebase.database().ref("accounts/").once("value").then(function (snapshot) {
 
             var foundMatchingUser = false;
@@ -52,7 +71,9 @@ var ConfigureLogin = function () {
                 {
                     document.getElementById("loginDiv").style["background-color"] = "green";
                     document.getElementById("logNotification").innerHTML = "<p>Password Correct.</p>";
-                    // window.location.href = "Index.html";
+                    window.location.href = "Homepage_Mockup/Index.html";
+
+
                 }
                 else
                 {
@@ -65,15 +86,155 @@ var ConfigureLogin = function () {
                 document.getElementById("loginDiv").style["background-color"] = "#fd7871";
                 document.getElementById("logNotification").innerHTML = "<p>User Does Not Exist.</p>";
             }
+
         })
     }
 }
+
+
+
+var ConfigureForgotUsername = function(){
+    var email = document.getElementById("fuEmail");
+    var securityText = document.getElementById("fuSecurityText");
+    var username = document.getElementById("newUsername");
+    var submit = document.getElementById("fuSubmit");
+    var cancel = document.getElementById("fuCancel");
+
+    cancel.onclick = function () {
+        document.getElementById("forgotUsernameDiv").style["display"] = "none";
+        document.getElementById("loginDiv").style["display"] = "block";
+
+        email.value = "";
+        securityText.value = "";
+    }
+
+        submit.onclick = function () {
+            if(email.value == "" || securityText.value == "" || username.value == "")
+                return;
+
+            firebase.database().ref("accounts/").once("value").then(function (snapshot) {
+
+                var foundMatchingUser = false;
+                var matchingUser = "";
+
+
+                snapshot.forEach(function (child) {
+                    var newItemValue = child.val();
+
+                    if (newItemValue.email == email.value) {
+                        foundMatchingUser = true;
+                        matchingUser = newItemValue;
+                    }
+                    if (matchingUser.username == username.value) {
+                        document.getElementById("forgotUsernameDiv").style["background-color"] = "#fd7871";
+                        document.getElementById("fuNotification").innerHTML = "<p>Username already exists.</p>";
+                    }
+
+                    else
+                        {
+
+
+                            if (foundMatchingUser) {
+                                if (matchingUser.securityText == securityText.value) {
+                                    firebase.database().ref("accounts/" + username.value).set({
+                                        username: username.value
+                                    });
+                                    document.getElementById("forgotUsernameDiv").style["background-color"] = "green";
+                                    document.getElementById("fuNotification").innerHTML = "<p>Username changed</p>";
+                                    document.getElementById("forgotUsernameDiv").style["display"] = "none";
+                                    document.getElementById("loginDiv").style["display"] = "block";
+                                }
+                                else {
+                                    document.getElementById("forgotUsernameDiv").style["background-color"] = "#fd7871";
+                                    document.getElementById("fuNotification").innerHTML = "<p>Incorrect security question.</p>";
+                                }
+                            }
+                            else {
+                                document.getElementById("forgotUsernameDiv").style["background-color"] = "#fd7871";
+                                document.getElementById("fuNotification").innerHTML = "<p>User Does Not Exist.</p>";
+                            }
+                        }
+                });
+
+            })
+        }
+    }
+
+
+
+
+var ConfigureForgotPassword = function(){
+    var email = document.getElementById("fpEmail");
+    var securityText = document.getElementById("fpSecurityText");
+    var password = document.getElementById("newPassword");
+    var submit = document.getElementById("fpSubmit");
+    var cancel = document.getElementById("fpCancel");
+
+    cancel.onclick = function () {
+        document.getElementById("forgotPasswordDiv").style["display"] = "none";
+        document.getElementById("loginDiv").style["display"] = "block";
+
+        email.value = "";
+        securityText.value = "";
+    }
+
+    submit.onclick = function () {
+        if(email.value == "" || securityText.value == "")
+            return;
+
+        firebase.database().ref("accounts/").once("value").then(function (snapshot) {
+
+            var foundMatchingUser = false;
+            var matchingUser = "";
+
+            snapshot.forEach(function (child) {
+                var newItemValue = child.val();
+
+                if(newItemValue.email == email.value) {
+                    foundMatchingUser = true;
+                    matchingUser = newItemValue;
+                }
+
+                if(foundMatchingUser)
+                {
+                    if(matchingUser.securityText == securityText.value)
+                    {
+                        firebase.database().ref("accounts/" + username.value).set({
+                            password: password.value
+                        });
+                        document.getElementById("forgotPasswordDiv").style["background-color"] = "green";
+                        document.getElementById("fpNotification").innerHTML = "<p>Password changed</p>";
+                        document.getElementById("forgotPasswordDiv").style["display"] = "none";
+                        document.getElementById("loginDiv").style["display"] = "block";
+                    }
+                    else
+                    {
+                        document.getElementById("forgotPasswordDiv").style["background-color"] = "#fd7871";
+                        document.getElementById("fpNotification").innerHTML = "<p>Incorrect security question.</p>";
+                    }
+                }
+                else
+                {
+                    document.getElementById("forgotPasswordDiv").style["background-color"] = "#fd7871";
+                    document.getElementById("fpNotification").innerHTML = "<p>User Does Not Exist.</p>";
+                }
+            });
+
+
+        })
+
+
+    }
+
+}
+
 
 var ConfigureRegister = function () {
     var username = document.getElementById("regUsername");
     var email = document.getElementById("regEmail");
     var password = document.getElementById("regPassword");
     var conPassword = document.getElementById("regConPassword");
+    var securityText = document.getElementById("regSecurityText");
     var submit = document.getElementById("regSubmit");
     var cancel = document.getElementById("regCancel");
 
@@ -85,10 +246,11 @@ var ConfigureRegister = function () {
         email.value = "";
         password.value = "";
         conPassword.value = "";
+        securityText.value = "";
     }
 
     submit.onclick = function () {
-        if(username.value == "" || email.value == "" || password.value == "" || conPassword.value == "")
+        if(username.value == "" || email.value == "" || password.value == "" || conPassword.value == "" || securityText == "")
             return;
 
         firebase.database().ref("accounts/").once("value").then(function (snapshot) {
@@ -129,9 +291,11 @@ var ConfigureRegister = function () {
                 if(password.value === conPassword.value)
                 {
                     firebase.database().ref("accounts/" + username.value).set({
-                    username: username.value,
-                    password: password.value,
-                    email: email.value
+                        username: username.value,
+                        password: password.value,
+                        email: email.value,
+                        securityText: securityText.value
+
                     });
 
                     document.getElementById("regNotification").innerHTML = "<p>Account Created.</p>";
