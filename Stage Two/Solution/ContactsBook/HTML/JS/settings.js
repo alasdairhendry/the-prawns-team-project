7ebbbc48e766c6773327fcb1279fd1910f3053dd
Thanos Kalogiratos <thanosk97@gmail.com>
2017-11-29 22:25:04 +0000

    fe

---
+++ Stage Two/Solution/ContactsBook/HTML/JS/settings.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+    CheckURL();
+}
+
+var CheckURL = function () {
+
+    var url = document.location.href;
+
+    // Make Sure we have a username, or return to login page
+    if(!url.includes("name="))
+    {
+        console.log("Does not include name tag");
+        document.location.href = "../UserValidation.html";
+    }
+
+    var params = url.split('?')[1].split('&'),
+        data = {}, tmp;
+
+    for (var i = 0, l = params.length; i < l; i++) {
+        tmp = params[i].split('=');
+        data[tmp[0]] = tmp[1];
+    }
+
+    // We made it this far so we have a username, Call Login for that username
+    Login(data.name);
+}
+
+var OnLoginSuccess = function () {
+    console.log("Logged In");
+}
+
+
+var clear = function(){
+            loggedInAccount.contacts.splice(0, loggedInAccount.contacts.Length);
+}
